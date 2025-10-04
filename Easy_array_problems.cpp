885f918fbd23f35c48c30a36375c34fd8dac1278
KRISHNA-VAID-20 <jrhixor@gmail.com>
2025-10-04 10:13:58 +0530

    Removed Unwanted

--- Easy_array_problems.cpp
+++ Easy_array_problems.cpp
@@ -59,6 +59,7 @@
 // }
 
 // //2nd smallest + smallestt
+
 // #include <iostream>
 // using namespace std;
 
@@ -146,6 +147,7 @@
 // }
 
 // //using set 
+
 // #include <bits/stdc++.h>
 // using namespace std;
 
@@ -219,148 +221,7 @@
 //     return 0;
 // }
 
-// //revision 
-// //largest element
-
-// #include <iostream>
-// using namespace std;
-
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >> n;
-//     int arr[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr[i];
-//     }
-//     int max=arr[0];
-//     for(int i=1;i<n;i++){
-//         if(arr[i]> max){
-//             max=arr[i];
-//         }
-//     }
-//     cout << "Max : " << max;
-//     return 0;
-// }
-
-// //2 second largest without soritng
-
-// #include <iostream>
-// using namespace std;
-
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >> n;
-//     int arr[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr[i];
-//     }
-//     int largest=arr[0];
-//     int slargest=-1;
-//     for(int i=0;i<n;i++){
-//         if(arr[i]>largest){
-//             slargest=largest;
-//             largest=arr[i];
-//         }
-//         if(arr[i] < largest & arr[i] > slargest){
-//             slargest=arr[i];
-//         }
-//     }
-//     cout << "Largest : " << largest << endl;
-//     cout << "Second Largest : " << slargest;
-//     return 0;
-// }
-
-// //3 check array is sorted
-
-// #include <iostream>
-// using namespace std;
-
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >> n;
-//     int arr[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr[i];
-//     }
-//     int is_sorted=1;
-//     for(int i=1;i<n;i++){
-//         if(arr[i]>=arr[i-1]){
-//         }
-//         else{
-//             is_sorted=0;
-//         }
-//     }
-//     if(is_sorted){
-//         cout << "Sorted";
-//     }
-//     else{
-//         cout << "Not sorted";
-//     }
-//     return 0;
-// }
-
-// //4 -remove duplicates from array
-
-// #include <iostream>
-// using namespace std;
-
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >> n;
-//     int arr[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr[i];
-//     }
-//     int i=0;
-//     for(int j=1;j<n;j++){
-//         if(arr[i] != arr[j]){
-//             arr[i+1]=arr[j];
-//             i++;
-//         }
-//     }
-//     i=i+1;
-//     cout << "Unique elements : " << i;
-//     return 0;
-
-// }
-
-// //5 Left rortate array by 1 place
-
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >> n;
-//     int arr[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr[i];
-//     }
-//     int temp=arr[0];
-
-//     for(int i=1;i<n;i++){
-//         arr[i-1]=arr[i];
-//     }
-//     arr[n-1]=temp;
-//     cout << "New array : " ;
-//     for(int i=0;i<n;i++){
-//         cout << arr[i] << " ";
-//     }
-//     return 0;
-// }
-
- 
-// move all zeroes to end 
+//7-  move all zeroes to end 
 
 // Brute force
 
