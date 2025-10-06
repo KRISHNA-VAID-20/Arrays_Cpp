d68f8d7797dd06d454147214beb088fcb77c10df
Im_Kv_ <jrhixor@gmail.com>
2025-10-06 19:46:19 +0530

    Optimal approach for Union problem

--- Easy_array_problems.cpp
+++ Easy_array_problems.cpp
@@ -1,370 +1,370 @@
-// // // 1 Largest element in array 
+// // 1 Largest element in array 
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >> n;
-//     int arr1[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr1[i];
-//     }
-//     int max,min;
-//     max=min=arr1[0];
-//     for(int i=0;i<n;i++){
-//         if(arr1[i]>max){
-//             max=arr1[i];
-//         }
-//         if(arr1[i]<min){
-//             min=arr1[i];
-//         }
-//     }
-//     cout << "Max : " << max;
-//     cout << "\nMin : " << min;
-//     return 0;
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    int arr1[n];
+    cout << "Enter " << n << " elements : ";
+    for(int i=0;i<n;i++){
+        cin >> arr1[i];
+    }
+    int max,min;
+    max=min=arr1[0];
+    for(int i=0;i<n;i++){
+        if(arr1[i]>max){
+            max=arr1[i];
+        }
+        if(arr1[i]<min){
+            min=arr1[i];
+        }
+    }
+    cout << "Max : " << max;
+    cout << "\nMin : " << min;
+    return 0;
 
-// }
+}
 
-// // // 2 Second smallest element
+// // 2 Second smallest element
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >> n;
-//     int arr1[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr1[i];
-//     }
-//     int smallest=arr1[0];
-//     int slargest=-1;
-//     for(int i=1;i<n;i++){
-//         if(arr1[i] > smallest){
-//             slargest=smallest;
-//             smallest=arr1[i];
-//         }
-//         else if(arr1[i] < smallest && arr1[i] > slargest){
-//             slargest=arr1[i];
-//         }
-//     }
-//     cout << "Largest : " << smallest;
-//     cout << "\nSecond smallest : " << slargest;
-//     return 0;
-// }
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    int arr1[n];
+    cout << "Enter " << n << " elements : ";
+    for(int i=0;i<n;i++){
+        cin >> arr1[i];
+    }
+    int smallest=arr1[0];
+    int slargest=-1;
+    for(int i=1;i<n;i++){
+        if(arr1[i] > smallest){
+            slargest=smallest;
+            smallest=arr1[i];
+        }
+        else if(arr1[i] < smallest && arr1[i] > slargest){
+            slargest=arr1[i];
+        }
+    }
+    cout << "Largest : " << smallest;
+    cout << "\nSecond smallest : " << slargest;
+    return 0;
+}
 
-// // //2nd smallest + smallestt
+// //2nd smallest + smallestt
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >> n;
-//     int arr1[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr1[i];
-//     }
-//     int smallest=arr1[0];
-//     int ssmallest=100000;
-//     for(int i=1;i<n;i++){
-//         if(arr1[i] < smallest){
-//             ssmallest=smallest;
-//             smallest=arr1[i];
-//         }
-//         else if(arr1[i] > smallest && arr1[i] < ssmallest){
-//             ssmallest=arr1[i];
-//         }
-//     }
-//     cout << "Smallest : " << smallest;
-//     cout << "\nSecond Smallest : " << ssmallest;
-//     return 0;
-// }
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    int arr1[n];
+    cout << "Enter " << n << " elements : ";
+    for(int i=0;i<n;i++){
+        cin >> arr1[i];
+    }
+    int smallest=arr1[0];
+    int ssmallest=100000;
+    for(int i=1;i<n;i++){
+        if(arr1[i] < smallest){
+            ssmallest=smallest;
+            smallest=arr1[i];
+        }
+        else if(arr1[i] > smallest && arr1[i] < ssmallest){
+            ssmallest=arr1[i];
+        }
+    }
+    cout << "Smallest : " << smallest;
+    cout << "\nSecond Smallest : " << ssmallest;
+    return 0;
+}
 
-// // //3 check arry is sorted ??
+// //3 check arry is sorted ??
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >> n;
-//     int arr1[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr1[i];
-//     }
-//     int is_sorted=1;
-//     for(int i=1;i<n;i++){
-//         if(arr1[i]>=arr1[i-1]){
-//         }
-//         else{
-//             is_sorted=0;
-//         }
-//     }
-//     if(is_sorted==0){
-//         cout << "not sorted Array ";
-//     }
-//     else{
-//         cout << "Sorted";
-//     }
-//     return 0;
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    int arr1[n];
+    cout << "Enter " << n << " elements : ";
+    for(int i=0;i<n;i++){
+        cin >> arr1[i];
+    }
+    int is_sorted=1;
+    for(int i=1;i<n;i++){
+        if(arr1[i]>=arr1[i-1]){
+        }
+        else{
+            is_sorted=0;
+        }
+    }
+    if(is_sorted==0){
+        cout << "not sorted Array ";
+    }
+    else{
+        cout << "Sorted";
+    }
+    return 0;
 
-// }
+}
 
-// // //4 Remove duplicates from array 
+// //4 Remove duplicates from array 
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >> n;
-//     int arr1[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr1[i];
-//     }
-//     int i=0;
-//     for(int j=1;j<n;j++){
-//         if(arr1[i] != arr1[j]){
-//             arr1[i+1]=arr1[j];
-//             i++;
-//         }
-//     }
-//     i=i+1;
-//     cout << "Unique elements : " << i;
-//     return 0;
-// }
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    int arr1[n];
+    cout << "Enter " << n << " elements : ";
+    for(int i=0;i<n;i++){
+        cin >> arr1[i];
+    }
+    int i=0;
+    for(int j=1;j<n;j++){
+        if(arr1[i] != arr1[j]){
+            arr1[i+1]=arr1[j];
+            i++;
+        }
+    }
+    i=i+1;
+    cout << "Unique elements : " << i;
+    return 0;
+}
 
-// // //using set 
+// //using set 
 
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >> n;
-//     int arr1[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr1[i];
-//     }
-//     cout << "Array Entered : ";
-//     for(int i=0;i<n;i++){
-//         cout << arr1[i] << " ";
-//     }
-//     set <int > st ;
-//     for(int i=0;i<n;i++){
-//         st.insert(arr1[i]);
-//     }
-//     cout <<"\nNew array : ";
-//     for(auto it : st){
-//         cout  <<  it << " ";
-//     }
-//     return 0;
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    int arr1[n];
+    cout << "Enter " << n << " elements : ";
+    for(int i=0;i<n;i++){
+        cin >> arr1[i];
+    }
+    cout << "Array Entered : ";
+    for(int i=0;i<n;i++){
+        cout << arr1[i] << " ";
+    }
+    set <int > st ;
+    for(int i=0;i<n;i++){
+        st.insert(arr1[i]);
+    }
+    cout <<"\nNew array : ";
+    for(auto it : st){
+        cout  <<  it << " ";
+    }
+    return 0;
 
-// }
+}
 
-// // //5 - Left rotate by one place 
+// //5 - Left rotate by one place 
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >> n;
-//     int arr1[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr1[i];
-//     }
-//     int temp=arr1[0];
-//     for(int i=1;i<n;i++){
-//         arr1[i-1]=arr1[i];
-//     }
-//     arr1[n-1]=temp;
-//     cout << "New Array : ";
-//     for(int i=0;i<n;i++){
-//         cout << arr1[i] << " ";
-//     }
-//     return 0;
-// }
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    int arr1[n];
+    cout << "Enter " << n << " elements : ";
+    for(int i=0;i<n;i++){
+        cin >> arr1[i];
+    }
+    int temp=arr1[0];
+    for(int i=1;i<n;i++){
+        arr1[i-1]=arr1[i];
+    }
+    arr1[n-1]=temp;
+    cout << "New Array : ";
+    for(int i=0;i<n;i++){
+        cout << arr1[i] << " ";
+    }
+    return 0;
+}
 
-// // //6 Left rotate by D places
+// //6 Left rotate by D places
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >> n;
-//     int arr1[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr1[i];
-//     }
-//     cout << "Rotated Array : ";
-//     for(int i=n-1;i>=0;i--){
-//         cout << arr1[i] << " ";
-//     }
-//     return 0;
-// }
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    int arr1[n];
+    cout << "Enter " << n << " elements : ";
+    for(int i=0;i<n;i++){
+        cin >> arr1[i];
+    }
+    cout << "Rotated Array : ";
+    for(int i=n-1;i>=0;i--){
+        cout << arr1[i] << " ";
+    }
+    return 0;
+}
 
-// //7-  move all zeroes to end 
+//7-  move all zeroes to end 
 
-// // Brute force
+// Brute force
 
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin>> n;
-//     int arr1[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr1[i];
-//     }
-//     vector <int> temp;
-//     for(int i=0;i<n;i++){
-//         if(arr1[i]!=0){
-//             temp.push_back(arr1[i]);
-//         }
-//     }
-//     int nonz=temp.size();
-//     for(int i=0;i<nonz;i++){
-//         arr1[i]=temp[i];
-//     }
-//     cout << "New array : ";
-//     for(int i=nonz;i<n;i++){
-//         arr1[i] =0;
-//     }
-//     for(int i=0;i<n;i++){
-//         cout << arr1[i] << " ";
-//     }
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin>> n;
+    int arr1[n];
+    cout << "Enter " << n << " elements : ";
+    for(int i=0;i<n;i++){
+        cin >> arr1[i];
+    }
+    vector <int> temp;
+    for(int i=0;i<n;i++){
+        if(arr1[i]!=0){
+            temp.push_back(arr1[i]);
+        }
+    }
+    int nonz=temp.size();
+    for(int i=0;i<nonz;i++){
+        arr1[i]=temp[i];
+    }
+    cout << "New array : ";
+    for(int i=nonz;i<n;i++){
+        arr1[i] =0;
+    }
+    for(int i=0;i<n;i++){
+        cout << arr1[i] << " ";
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
-// // OPTIMAL APPROACH FOR SAME QUESTION 
+// OPTIMAL APPROACH FOR SAME QUESTION 
 
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin>> n;
-//     int arr1[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr1[i];
-//     }
-//     int j=-1;
-//     for(int i=0;i<n;i++){
-//         if(arr1[i]==0){
-//             j=i;
-//             break;
-//         }
-//     }
-//     for(int i=j+1;i<n;i++){
-//         if(arr1[i]!=0){
-//             swap(arr1[i],arr1[j]);
-//             j++;
-//         }
-//     }
-//     cout << "New array : ";
-//     for(int i=0;i<n;i++){
-//         cout << arr1[i] << " ";
-//     }
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin>> n;
+    int arr1[n];
+    cout << "Enter " << n << " elements : ";
+    for(int i=0;i<n;i++){
+        cin >> arr1[i];
+    }
+    int j=-1;
+    for(int i=0;i<n;i++){
+        if(arr1[i]==0){
+            j=i;
+            break;
+        }
+    }
+    for(int i=j+1;i<n;i++){
+        if(arr1[i]!=0){
+            swap(arr1[i],arr1[j]);
+            j++;
+        }
+    }
+    cout << "New array : ";
+    for(int i=0;i<n;i++){
+        cout << arr1[i] << " ";
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
-// // Linear Search 
+// Linear Search 
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >>n;
-//     int arr1[n];
-//     cout << "Enter " << n << " elements : ";
-//     for(int i=0;i<n;i++){
-//         cin >> arr1[i];
-//     }
-//     int key;
-//     cout << "Enter number to search : ";
-//     cin >> key;
-//     for(int i=0;i<n;i++){
-//         if(arr1[i]==key){
-//             cout << "Element "<< key << " found at index : "<< i;
-//             break;
-//         }
-//         else{
-//             cout << key << " Not found ";
-//             return -1;
-//         }
-//     }
-//     return 0;
-// }
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >>n;
+    int arr1[n];
+    cout << "Enter " << n << " elements : ";
+    for(int i=0;i<n;i++){
+        cin >> arr1[i];
+    }
+    int key;
+    cout << "Enter number to search : ";
+    cin >> key;
+    for(int i=0;i<n;i++){
+        if(arr1[i]==key){
+            cout << "Element "<< key << " found at index : "<< i;
+            break;
+        }
+        else{
+            cout << key << " Not found ";
+            return -1;
+        }
+    }
+    return 0;
+}
 
-// // UNION of two sorted arrays  [ BRUTE FORCE ]
+// UNION of two sorted arrays  [ BRUTE FORCE ]
 
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n1,n2;
-//     cout << "Enter n1 : ";
-//     cin >> n1;
-//     int arr1[n1];
-//     cout <<"Array 1 - " <<"Enter "<< n1 << " elements : ";
-//     for(int i=0;i<n1;i++){
-//         cin >> arr1[i];
-//     }
-//     cout << "Enter n2 : ";
-//     cin >> n2;
-//     int arr2[n2];
-//     cout <<"Array 2 - " <<"Enter "<< n2 << " elements : ";
-//     for(int i=0;i<n2;i++){
-//         cin >> arr2[i];
-//     }
-//     set <int> st;
+int main(){
+    int n1,n2;
+    cout << "Enter n1 : ";
+    cin >> n1;
+    int arr1[n1];
+    cout <<"Array 1 - " <<"Enter "<< n1 << " elements : ";
+    for(int i=0;i<n1;i++){
+        cin >> arr1[i];
+    }
+    cout << "Enter n2 : ";
+    cin >> n2;
+    int arr2[n2];
+    cout <<"Array 2 - " <<"Enter "<< n2 << " elements : ";
+    for(int i=0;i<n2;i++){
+        cin >> arr2[i];
+    }
+    set <int> st;
 
-//     for(int i=0;i<n1;i++){
-//         st.insert(arr1[i]);
-//     }
-//     for(int i=0;i<n2;i++){
-//         st.insert(arr2[i]);
-//     }
-//     int uni[st.size()];
-//     int i =0;
-//     for(auto it : st){
-//         uni[i++]=it;
-//     }
+    for(int i=0;i<n1;i++){
+        st.insert(arr1[i]);
+    }
+    for(int i=0;i<n2;i++){
+        st.insert(arr2[i]);
+    }
+    int uni[st.size()];
+    int i =0;
+    for(auto it : st){
+        uni[i++]=it;
+    }
 
-//     cout << "Union of Both : " ;
-//     for(int i=0;i<st.size();i++){
-//         cout << uni[i] << " ";
-//     }
-//     return 0;
+    cout << "Union of Both : " ;
+    for(int i=0;i<st.size();i++){
+        cout << uni[i] << " ";
+    }
+    return 0;
 
-// }
+}
 
 // // Optimal approach
 
