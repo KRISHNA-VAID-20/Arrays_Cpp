14c3624f7db99d58f36cd715f4471f69c763a9a4
Im_Kv_ <jrhixor@gmail.com>
2025-10-18 20:01:21 +0530

    Revision added

--- revision.cpp
+++ revision.cpp
@@ -1,25 +1,25 @@
 // //  largest elemnt in array
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cout << "Enter n : ";
-//     cin >>n;
-//     int arr[n];
-//     cout << "Enter " << n << " Elements : ";
-//     for(int  i=0;i<n;i++){
-//         cin >> arr[i];
-//     }
-//     int max=arr[0];
-//     for(int i=0;i<n;i++){
-//         if(arr[i]>max){
-//             max=arr[i];
-//         }
-//     }
-//     cout << "max : "<< max;
-//     return 0;
-// }
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >>n;
+    int arr[n];
+    cout << "Enter " << n << " Elements : ";
+    for(int  i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    int max=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    cout << "max : "<< max;
+    return 0;
+}
 
 // 2 !
 #include <iostream>
