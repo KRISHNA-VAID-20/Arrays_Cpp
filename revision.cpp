f1472e5adff2f1430cbaac101a8cc455202b39bb
Im_Kv_ <jrhixor@gmail.com>
2025-11-13 19:12:30 +0530

    Revision goin on

--- revision.cpp
+++ revision.cpp
@@ -129,3 +129,27 @@ int main(){
 
 
 }
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "N : ";
+    cin >>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    int hash[10]={0};
+    for(int i=0;i<n;i++){
+        hash[arr[i]]++;
+    }
+    for(int i=0;i<n;i++){
+        if(hash[arr[i]]==1){
+            cout << "Appeared once : " <<arr[i];
+
+        }
+        
+    }
+    return 0;
+}
