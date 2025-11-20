760d87ec05b669cdbb3882e68aa111ff88eee9da
user3 <user3@example.com>
2025-11-20 19:32:25 +0530

    Dutch National flag Algorithm

--- Medium_array_problems.cpp
+++ Medium_array_problems.cpp
@@ -80,3 +80,44 @@ int main(){
     }
     return 0;
 }
+
+// optimal approach using DUTCH NATIONAL FLAG ALGORITHM
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    cout << "Array entered : \n";
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    int low=0,mid=0,high=n-1;
+
+    for(int i=0;i<n;i++){
+        if(arr[mid]==0){
+            swap(arr[low],arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid]==1){
+            mid++;
+        }
+        else{
+            swap(arr[mid],arr[high]);
+            high--;
+        }
+        
+    }
+    cout << "\nSorted array : \n";
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
